4997371f680da131e36d15ee449928029cf9df70
Geek <you@example.com>
2020-03-05 15:16:23 +0200

    store - backend REST API completed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use App\Http\Resources\Product as ProductResource;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 class ProductController extends Controller
 {
@@ -14,18 +17,26 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::all();
+        return ProductResource::collection($products);
     }
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return ProductResource
      */
     public function store(Request $request)
     {
-        //
+        $product = new Product();
+
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->image_url = $request->input('image_url');
+        $product->save();
+
+        return new ProductResource($product);
     }
 
     /**
@@ -36,7 +47,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return new ProductResource($product);
     }
 
     /**
@@ -48,17 +59,35 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        if ($request->has('name')) {
+            $product->name = $request->input('name');
+        }
+        if ($request->has('description')) {
+            $product->description = $request->input('description');
+        }
+        if ($request->has('price')) {
+            $product->price = $request->input('price');
+        }
+        if ($request->has('image_url')) {
+            $product->image_url = $request->input('image_url');
+        }
+        $product->save();
+
+        return new ProductResource($product);
     }
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
+     * @param Product $product
+     * @return JsonResponse
+     * @throws \Exception
      */
     public function destroy(Product $product)
     {
-        //
+        if ($product->delete()) {
+            return new JsonResponse(['status' => 'success', 'msg' => 'Deleted Successfully']);
+        }
+
+        throw new \Exception('Product does not exist');
     }
 }
